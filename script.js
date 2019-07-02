0be14585917bfb231aa3265dadb5edec038f73ec
Cong Kim <congkim94@gmail.com>
2019-07-02 17:43:51 -0400

    initializing

---
+++ script.js
@@ -0,0 +1,21 @@
+(function() {
+    var hamburger = {
+        navToggle: document.querySelector('.menu-toggle'),
+        nav: document.querySelector('.box'),
+        doToggle: function(e) {
+            
+            this.navToggle.classList.toggle('active');
+            this.nav.classList.toggle('active');
+        }
+    };
+    hamburger.navToggle.addEventListener('click', function(e) {
+        hamburger.doToggle(e);
+    });
+    hamburger.nav.addEventListener('click', function(e) {
+        hamburger.doToggle(e);
+    });
+}());
+
+
+
+
